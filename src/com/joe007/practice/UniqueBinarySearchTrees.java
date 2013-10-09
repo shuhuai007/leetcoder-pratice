d157ac39e7e3f793899d3ec31bb5dfbdf30a33a8
jiezhou <jie_zhou@allyes.com>
2013-10-09 15:52:21 +0800

    UniqueBinarySearchTrees

--- src/com/joe007/practice/UniqueBinarySearchTrees.java
+++ src/com/joe007/practice/UniqueBinarySearchTrees.java
@@ -9,20 +9,40 @@ package com.joe007.practice;
      3     2     1      1   3      2
     /     /       \                 \
    2     1         2                 3
- *
+ * Start Time : 2013/10/09 : 14:40
+ * End   Time : 2013/10/09 
  *
  */
 public class UniqueBinarySearchTrees {
 
     public int numTrees(int n) {
+        if(0 == n){
+            return 0;
+        }
+        if(1 == n){
+            return 1;
+        }
+        int totalNum = 0;
 
-        
-        return 0;
+        for(int i = 0; i <= n-1; i++ ){
+            int leftChildNumTrees = numTrees(i);
+            int rightChildNumTrees = numTrees(n-1-i);
+
+            int temp = 0;
+            if(leftChildNumTrees == 0 || rightChildNumTrees == 0){
+                temp = leftChildNumTrees + rightChildNumTrees;
+            } else {
+                temp = leftChildNumTrees * rightChildNumTrees;
+            }
+            totalNum += temp;
+        }
+
+        return totalNum;
     }
 
     public static void main(String[] args) {
         UniqueBinarySearchTrees instance = new UniqueBinarySearchTrees();
-        System.out.println(instance.numTrees(10));
+        System.out.println(instance.numTrees(4));
     }
 
 }
