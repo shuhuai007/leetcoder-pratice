d3ecd081ea031a5231013f0513433d8d34a657ea
jiezhou <jie_zhou@allyes.com>
2013-10-24 15:25:19 +0800

    LongestCommonPrefix

--- src/com/joe007/practice/LongestCommonPrefix.java
+++ src/com/joe007/practice/LongestCommonPrefix.java
@@ -5,7 +5,7 @@ package com.joe007.practice;
  * @author zhoujie
  * 
  * Start Time : 2013/10/24 : 15:03
- * End   Time : 2013/10/24 :
+ * End   Time : 2013/10/24 : 15:24
  *
  */
 public class LongestCommonPrefix {
@@ -17,14 +17,46 @@ public class LongestCommonPrefix {
         if(strs.length == 1){
             return strs[0];
         }
+
+        String commonStr = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            String currentStr = strs[i];
+            if(currentStr.equals("")){
+                return "";
+            }
+            commonStr = compareStr(currentStr, commonStr);
+            if(commonStr.equals("")){
+                break;
+            }
+            
+        }
         
         
-        return null;
+        return commonStr;
     }
     
+    private String compareStr(String currentStr, String commonStr) {
+        int len1 = currentStr.length();
+        int len2 = commonStr.length();
+        int minLen = Math.min(len1, len2);
+        int flagPos = -1;
+        for(int i = 0; i < minLen; i++){
+            if(currentStr.charAt(i) != commonStr.charAt(i)){
+                break;
+            } else {
+                flagPos = i;
+            }
+        }
+        if(flagPos == -1){
+            return "";
+        }
+        String result = currentStr.substring(0, flagPos + 1);
+        return result;
+    }
+
     public static void main(String[] args) {
         LongestCommonPrefix instance = new LongestCommonPrefix();
-        String[] strs = {"good", "gomorning", "afternoon"};
+        String[] strs = {"good", "goomorning", "goodfternoon"};
         System.out.println(instance.longestCommonPrefix(strs));
     }
 
