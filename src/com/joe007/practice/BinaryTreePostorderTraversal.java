31536784b9b68736d121edd5f551091027e84411
Joe <zhoujie338@126.com>
2014-04-14 14:25:14 +0800

    binary tree postorder traversal---iterative method

--- src/com/joe007/practice/BinaryTreePostorderTraversal.java
+++ src/com/joe007/practice/BinaryTreePostorderTraversal.java
@@ -1,6 +1,7 @@
 package com.joe007.practice;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
 
@@ -14,23 +15,71 @@ public class BinaryTreePostorderTraversal {
         }
     }
 
+    class TreeNodeWrapper extends TreeNode {
+
+        boolean rightChildVisited;
+
+        TreeNodeWrapper(int x) {
+            super(x);
+            rightChildVisited = false;
+        }
+
+    }
+
+    // public ArrayList<Integer> postorderTraversal(TreeNode root) {
+    // ArrayList<Integer> resultList = new ArrayList<Integer>();
+    // if(root == null){
+    // return resultList;
+    // }
+    //
+    // if (root.left != null) {
+    // resultList.addAll(postorderTraversal(root.left));
+    // }
+    //
+    // if (root.right != null) {
+    // resultList.addAll(postorderTraversal(root.right));
+    // }
+    //
+    // resultList.add(root.val);
+    // return resultList;
+    // }
+
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> resultList = new ArrayList<Integer>();
-        if(root == null){
+        if (root == null) {
             return resultList;
         }
 
-        if (root.left != null) {
-            resultList.addAll(postorderTraversal(root.left));
-        }
+        TreeNode sentinelNode = new TreeNode(0);
+        sentinelNode.right = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(sentinelNode);
 
-        if (root.right != null) {
-            resultList.addAll(postorderTraversal(root.right));
-        }
+        TreeNode previousNode = null;
+        while (!stack.isEmpty()) {
+            TreeNode currentNode = stack.peek();
 
-        resultList.add(root.val);
+            if (currentNode.right != null && currentNode.right != previousNode) {
+                TreeNode temp = currentNode.right;
+                while (temp != null) {
+                    stack.push(temp);
+                    temp = temp.left;
+                }
+            } else {
+                stack.pop();
+                if(currentNode != sentinelNode){
+                    resultList.add(currentNode.val);
+                }
+                previousNode = currentNode;
+            }
+
+        }
         return resultList;
     }
+
+//    private boolean isLeaf(TreeNode currentNode) {
+//        return (currentNode.left == null && currentNode.right == null);
+//    }
 
     public static void main(String[] args) {
 
